6ae63a47d48a94e0df088471e1432695cf98b149
K-CHANHO <cksgh1565@gmail.com>
2023-12-12 09:37:07 +0900

    git init

---
+++ src/test/java/com/example/playWorld/PlayWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
