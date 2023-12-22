89a965f1cc99ef5b0fa762ef502706f018014900
K-CHANHO <cksgh1565@gmail.com>
2023-12-22 16:54:19 +0900

    커밋

--- src/main/java/com/example/playWorld/play/PlayController.java
+++ src/main/java/com/example/playWorld/play/PlayController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +21,6 @@ public class PlayController {
         PlayListResultDTO playList = playService.getPlayList(requestDTO);
 
         return new ResponseEntity(playList, HttpStatus.OK);
-
     }
 
 }
